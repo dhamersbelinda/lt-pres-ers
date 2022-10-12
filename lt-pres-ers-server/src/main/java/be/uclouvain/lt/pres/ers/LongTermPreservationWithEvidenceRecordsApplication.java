62e94bedce5489433d8ccc3ba4c2068fdc93c152
jlepropre <jean.lepropre@nowina.lu>
2022-10-12 15:40:29 +0200

    Add @ComponentScan

--- lt-pres-ers-server/src/main/java/be/uclouvain/lt/pres/ers/LongTermPreservationWithEvidenceRecordsApplication.java
+++ lt-pres-ers-server/src/main/java/be/uclouvain/lt/pres/ers/LongTermPreservationWithEvidenceRecordsApplication.java
@@ -2,8 +2,10 @@ package be.uclouvain.lt.pres.ers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "be.uclouvain.lt.pres.ers", "org.openapitools.configuration" })
 public class LongTermPreservationWithEvidenceRecordsApplication {
 
     public static void main(final String[] args) {

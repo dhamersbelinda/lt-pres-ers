751b54811c572d25c92cc366d308cf90f5735db0
jlepropre <jean.lepropre@nowina.lu>
2022-10-14 15:52:49 +0200

    Enums

--- lt-pres-ers-model/src/main/java/be/uclouvain/lt/pres/ers/model/PreservationStorageModel.java
+++ lt-pres-ers-model/src/main/java/be/uclouvain/lt/pres/ers/model/PreservationStorageModel.java
@@ -1,17 +1,27 @@
 package be.uclouvain.lt.pres.ers.model;
 
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum PreservationStorageModel {
 
     WITH_STORAGE("WithStorage"), WITH_TEMPORARY_STORAGE("WithTemporaryStorage"), WITHOUT_STORAGE("WithoutStorage");
 
-    private String value;
+    @Getter
+    private String standardizedValue;
 
-    private PreservationStorageModel(final String value) {
-        this.value = value;
+    private static final Map<String, PreservationStorageModel> fromStandardizedValuesToInstances = Map.of("WithStorage",
+            WITH_STORAGE, "WithTemporaryStorage", WITH_TEMPORARY_STORAGE, "WithoutStorage", WITHOUT_STORAGE);
+
+    public static PreservationStorageModel fromStandardizedValue(final String standardizedValue) {
+        if (fromStandardizedValuesToInstances.containsKey(standardizedValue)) {
+            return fromStandardizedValuesToInstances.get(standardizedValue);
+        } else {
+            throw new IllegalArgumentException(
+                    "No PreservationStorageModel enum constant with standardized value: " + standardizedValue);
+        }
     }
-
-    public String getValue() {
-        return this.value;
-    }
-
 }
--- lt-pres-ers-model/src/main/java/be/uclouvain/lt/pres/ers/model/ProfileStatus.java
+++ lt-pres-ers-model/src/main/java/be/uclouvain/lt/pres/ers/model/ProfileStatus.java
@@ -1,7 +1,27 @@
 package be.uclouvain.lt.pres.ers.model;
 
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum ProfileStatus {
 
-    ACTIVE, INACTIVE, ALL;
+    ACTIVE("active"), INACTIVE("inactive"), ALL("all");
 
+    @Getter
+    private String standardizedValue;
+
+    private static final Map<String, ProfileStatus> fromStandardizedValuesToInstances = Map.of("active", ACTIVE,
+            "inactive", INACTIVE, "all", ALL);
+
+    public static ProfileStatus fromStandardizedValue(final String standardizedValue) {
+        if (fromStandardizedValuesToInstances.containsKey(standardizedValue)) {
+            return fromStandardizedValuesToInstances.get(standardizedValue);
+        } else {
+            throw new IllegalArgumentException(
+                    "No ProfileStatus enum constant with standardized value: " + standardizedValue);
+        }
+    }
 }

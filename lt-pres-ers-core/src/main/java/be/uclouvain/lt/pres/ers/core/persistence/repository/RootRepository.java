de3b92b0795ba9a722253fab10fd9a9c4ae2eab5
Sasha Broux <sasha.broux@student.uclouvain.be>
2023-02-13 11:11:54 +0100

    Rollback changelog with PO_ID in DIGESTLIST, added RootRepository

---
+++ lt-pres-ers-core/src/main/java/be/uclouvain/lt/pres/ers/core/persistence/repository/RootRepository.java
@@ -0,0 +1,8 @@
+package be.uclouvain.lt.pres.ers.core.persistence.repository;
+
+import be.uclouvain.lt.pres.ers.core.persistence.model.Node;
+import be.uclouvain.lt.pres.ers.core.persistence.model.Root;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RootRepository extends CrudRepository<Root, Node> {
+}

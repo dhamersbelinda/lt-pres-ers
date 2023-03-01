0361321d85e6ddc4340d3fa5d757ccd317b74d4e
Sasha Broux <sasha.broux@student.uclouvain.be>
2023-03-01 10:11:14 +0100

    Client entity to match table

---
+++ lt-pres-ers-core/src/main/java/be/uclouvain/lt/pres/ers/core/persistence/model/Client.java
@@ -0,0 +1,19 @@
+package be.uclouvain.lt.pres.ers.core.persistence.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Client {
+    @Id
+    @Column(name = "CLIENT_ID")
+    @Setter(value = AccessLevel.PRIVATE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long clientId;
+}

8f79d0091ead1ee50567108d20afffc0ad45c6db
Sasha Broux <sasha.broux@student.uclouvain.be>
2023-03-26 13:18:56 +0200

    improved in case of null value

--- lt-pres-ers-utils/src/main/java/be/uclouvain/lt/pres/ers/utils/ByteUtils.java
+++ lt-pres-ers-utils/src/main/java/be/uclouvain/lt/pres/ers/utils/ByteUtils.java
@@ -1,12 +1,15 @@
 package be.uclouvain.lt.pres.ers.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ByteUtils {
 
     public static byte[] concat(List<byte[]> byteArrays) {
-        int size = 0;
+        int size = 0, i = 0;
         for (byte[] bytes : byteArrays) {
+            if(bytes == null) throw new NullPointerException("Cannot invoke bytes.length as bytes is null, it was the "+i+"th argument.");
             size += bytes.length;
         }
         byte[] concat = new byte[size];
@@ -16,6 +19,11 @@ public class ByteUtils {
             runner += bytes.length;
         }
         return concat;
+    }
+
+    public static byte[] concat(byte[]... byteArrays) {
+        List<byte[]> args = new ArrayList<>(Arrays.asList(byteArrays));
+        return concat(args);
     }
 
 }
